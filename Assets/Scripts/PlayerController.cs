1e104f8a87883a4fcc7697760df2ef490d52cc1a
JnsRdnp <112471004+JnsRdnp@users.noreply.github.com>
2024-11-05 12:48:21 +0200

    Player controller added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,15 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] float movementSpeed = 1f;
 
-
+    [SerializeField] Rigidbody2D playerRigidbody;
+    private Animator playerAnimator;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        playerAnimator = GetComponent<Animator>();
     }
-
+    
     // Update is called once per frame
     void Update()
     {
