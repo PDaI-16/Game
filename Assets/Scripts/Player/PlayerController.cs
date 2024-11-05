ca7e8a7bc77191a511c67e78545cef9e42e288bd
unknown <joonasroska123@gmail.com>
2024-11-05 18:34:00 +0200

    Animations relative to movement started

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,20 +2,64 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float movementSpeed = 1f;
 
     [SerializeField] Rigidbody2D playerRigidbody;
-    private Animator playerAnimator;
+    [SerializeField] int movementSpeed;
+    
+    
+    private Vector2 _movementInput;
+    private Animator _playerAnimator;
+    private Camera _mainCamera;
+
+    private Vector3 _mousePosition;
+    private Vector3 _screenPoint;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        playerAnimator = GetComponent<Animator>();
-    }
-    
-    // Update is called once per frame
+        _mainCamera = Camera.main;
+        movementSpeed = 2;
+        _playerAnimator = GetComponent<Animator>();
+        
+    } // Update is called once per frame
     void Update()
     {
-        
+        _movementInput.x = Input.GetAxisRaw("Horizontal");
+        _movementInput.y = Input.GetAxisRaw("Vertical");
+
+        _movementInput.Normalize(); // Normalizing the movement input so that diagonal movement is not faster than others.
+
+        playerRigidbody.linearVelocity = _movementInput * movementSpeed;
+
+        _mousePosition = Input.mousePosition;
+        _screenPoint = _mainCamera.WorldToScreenPoint(transform.localPosition);
+
+        FlipPlayer();
+        AnimationHandler();
+       }
+
+    void AnimationHandler()
+    {
+        if(_movementInput != Vector2.zero)
+        {
+            _playerAnimator.SetBool("isWalking", true);
+        }
+        else
+        {
+            _playerAnimator.SetBool("isWalking", false);
+        }
+    }
+    void FlipPlayer()
+    {
+
+        if (_mousePosition.x < _screenPoint.x)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }            
     }
 }
