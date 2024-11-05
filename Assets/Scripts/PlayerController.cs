a5f1839d1c0626262607d6d489a7584991f47097
JnsRdnp <112471004+JnsRdnp@users.noreply.github.com>
2024-11-05 12:09:08 +0200

    Basic animations for player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] float movementSpeed = 1f;
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
     }
 
     // Update is called once per frame
