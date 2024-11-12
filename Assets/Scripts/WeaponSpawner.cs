21211731035bf3746bedb60e4bb0f26dd6c7e77c
unknown <joonasroska123@gmail.com>
2024-11-12 15:13:37 +0200

    Random weapon from a list is shown

--- Assets/Scripts/WeaponSpawner.cs
+++ Assets/Scripts/WeaponSpawner.cs
@@ -6,15 +6,28 @@ public class WeaponSpawner : MonoBehaviour
 
     [SerializeField] Sprite[] weaponSprites;
 
-    
+    [SerializeField] GameObject weapon;
+
+    private SpriteRenderer weaponSpriteRenderer;
+
+    [SerializeField] Sprite chosenSprite;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+
+        //weapon = GameObject.Find("Weapon");
+
         int randomWeaponIndex = Random.Range(0, weaponSprites.Length);
+        chosenSprite = weaponSprites[randomWeaponIndex];
 
         print(weaponSprites[randomWeaponIndex]);
 
+        weaponSpriteRenderer = weapon.GetComponentInChildren<SpriteRenderer>();
+        weaponSpriteRenderer.sprite = weaponSprites[randomWeaponIndex];
+
+        Instantiate(weapon, new Vector3(0, 0, 0), Quaternion.identity);
     }
 
     // Update is called once per frame
