80cecbd76cdbac33382f88fc3b9b854793f136a9
JnsRdnp <112471004+JnsRdnp@users.noreply.github.com>
2024-11-13 12:09:24 +0200

    Add or remove weapons from inventory

--- Assets/Scripts/Inventory/InventoryController.cs
+++ Assets/Scripts/Inventory/InventoryController.cs
@@ -6,22 +6,17 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "InventoryController", menuName = "Scriptable Objects/InventoryController")]
 public class InventoryController : ScriptableObject
 {
-    public List<GameObject> weaponsInInventory;
-
-    void start()
-    {
-        weaponsInInventory = new List<GameObject>();
-    }
+    public List<GameObject> weaponsInInventory = new List<GameObject>();
+    public List<GameObject> hatsInInventory = new List<GameObject>(); // todo hats
+    
    
     void addWeapon(GameObject pickedUpWeapon) {
         weaponsInInventory.Add(pickedUpWeapon);
     }
  
-    void removeWeapon()
+    void removeWeapon(GameObject droppedWeapon)
     {
-
+        weaponsInInventory.Remove(droppedWeapon);
     }
-
-
 
 }
