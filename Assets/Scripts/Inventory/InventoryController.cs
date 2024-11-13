ceb860221872e71d4e800b3e42b9c6830fc48ae5
JnsRdnp <112471004+JnsRdnp@users.noreply.github.com>
2024-11-13 12:02:25 +0200

    Base for inventoryController

--- Assets/Scripts/Inventory/InventoryController.cs
+++ Assets/Scripts/Inventory/InventoryController.cs
@@ -1,17 +1,26 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "InventoryController", menuName = "Scriptable Objects/InventoryController")]
 public class InventoryController : ScriptableObject
 {
-    
+    public List<GameObject> weaponsInInventory;
 
+    void start()
+    {
+        weaponsInInventory = new List<GameObject>();
+    }
+   
+    void addWeapon(GameObject pickedUpWeapon) {
+        weaponsInInventory.Add(pickedUpWeapon);
+    }
+ 
+    void removeWeapon()
+    {
 
-
-
-
-
-
-
+    }
 
 
 
