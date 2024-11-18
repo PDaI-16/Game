891b38b36594cb0532c45e9a242dcc4b122e5327
JnsRdnp <112471004+JnsRdnp@users.noreply.github.com>
2024-11-18 20:15:36 +0200

    Weapon spawner cleaner

--- Assets/Scripts/Weapon/WeaponSpawner.cs
+++ Assets/Scripts/Weapon/WeaponSpawner.cs
@@ -2,47 +2,83 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
-/*https://docs.unity3d.com/6000.0/Documentation/Manual/instantiating-prefabs-intro.html
-https://discussions.unity.com/t/best-way-to-reference-a-prefab-game-object/935673/7
-https://discussions.unity.com/t/instantiating-gameobjects-at-random-screen-positions/633835 
-https://gamedevbeginner.com/how-to-make-an-inventory-system-in-unity/
+
+/*
+ * References:
+ * https://docs.unity3d.com/6000.0/Documentation/Manual/instantiating-prefabs-intro.html
+ * https://discussions.unity.com/t/best-way-to-reference-a-prefab-game-object/935673/7
+ * https://discussions.unity.com/t/instantiating-gameobjects-at-random-screen-positions/633835
+ * https://gamedevbeginner.com/how-to-make-an-inventory-system-in-unity/
  */
 
 public class WeaponSpawner : MonoBehaviour
 {
-    [SerializeField] private WeaponSO weaponSO;
-    [SerializeField] private GameObject weaponPrefab;
-    [SerializeField] private GameObject background;
-    [SerializeField] public WeaponData defaultWeaponData;
-    [SerializeField] public GameObject Inventory;
-    [SerializeField] public GameObject WeaponPrefab;
-    [SerializeField] public GameObject WeaponArm;
-    [SerializeField] private WeaponData weaponData;
+    [SerializeField] private WeaponSO _weaponSO;
+    [SerializeField] private GameObject _weaponPrefab;
+    [SerializeField] private GameObject _background;
+    [SerializeField] private WeaponData _defaultWeaponData;
+    [SerializeField] private GameObject _inventory;
 
-    [SerializeField] public GameObject Player;
-    [SerializeField] private PlayerWeaponController playerWeaponController;
+    private PlayerWeaponController _playerWeaponController;
+    private InventoryController _inventoryController;
 
-    [SerializeField] public InventoryController inventoryController;
-
-    public void Start()
+    private void Start()
     {
         StoreDefaultWeapon();
-/*        EquipDefaultWeapon();*/
         SpawnWeapon(5);
         SpawnWeapon(5);
     }
 
-    public void SpawnWeapon(int multiplier)
+    private void Update()
     {
-        // Get all random stats from WeaponSO
-        var (randomSprite, damage, attackSpeed, weaponScore) = weaponSO.GetRandomWeaponStats(multiplier);
+        // Automatically equip the latest picked weapon
+        if (_inventoryController != null && _inventoryController.weaponsInInventory.Count > 0)
+        {
+            var lastWeapon = _inventoryController.weaponsInInventory[^1]; // ^1 gets the last element
+            _playerWeaponController.SetPlayerWeapon(lastWeapon);
+        }
+    }
 
-        // Generate position
-        Vector3 spawnPosition = GetRandomSpawnPosition();
+    private void StoreDefaultWeapon()
+    {
+        Debug.Log("Creating default weapon and adding to inventory.");
+
+        var player = GameObject.FindWithTag("Player");
+        if (player == null)
+        {
+            Debug.LogError("Player not found in the scene.");
+            return;
+        }
+
+        _playerWeaponController = player.GetComponent<PlayerWeaponController>();
+        if (_playerWeaponController == null)
+        {
+            Debug.LogError("PlayerWeaponController script not found on Player.");
+            return;
+        }
+
+        _inventoryController = _inventory.GetComponent<InventoryController>();
+        if (_inventoryController == null)
+        {
+            Debug.LogError("InventoryController script not found on Inventory.");
+            return;
+        }
+
+        _inventoryController.AddWeapon(_defaultWeaponData);
+        _playerWeaponController.SetPlayerWeapon(_inventoryController.weaponsInInventory[0]);
+    }
+
+    private void SpawnWeapon(int multiplier)
+    {
+        // Get random stats from WeaponSO
+        var (randomSprite, damage, attackSpeed, weaponScore) = _weaponSO.GetRandomWeaponStats(multiplier);
+
+        // Generate a random spawn position
+        var spawnPosition = GetRandomSpawnPosition();
 
         // Instantiate the weapon and set its values
-        GameObject weaponInstance = Instantiate(weaponPrefab, spawnPosition, Quaternion.identity);
-        Weapon weaponScript = weaponInstance.GetComponent<Weapon>();
+        var weaponInstance = Instantiate(_weaponPrefab, spawnPosition, Quaternion.identity);
+        var weaponScript = weaponInstance.GetComponent<Weapon>();
 
         if (weaponScript != null)
         {
@@ -54,43 +90,23 @@ public class WeaponSpawner : MonoBehaviour
         }
     }
 
-
-    private void StoreDefaultWeapon()
-    {
-        /*        defaultWeaponData = new WeaponData();*/
-        print("Create default weapon to inventory");
-
-        Player = GameObject.FindWithTag("Player");
-        playerWeaponController = Player.GetComponent<PlayerWeaponController>();
-
-        inventoryController = Inventory.GetComponent<InventoryController>();
-        if (inventoryController != null)
-        {
-            inventoryController.AddWeapon(defaultWeaponData);
-        }
-
-        playerWeaponController.SetPlayerWeapon(inventoryController.weaponsInInventory[0]);
-       
-
-    }
-
-    private void Update()
-    {  
-        // Just for testing, changes to latest picked weapon automatically...
-        if (inventoryController.weaponsInInventory.Count > 0) // Check if there is at least one weapon
-        {
-            playerWeaponController.SetPlayerWeapon(
-                inventoryController.weaponsInInventory[inventoryController.weaponsInInventory.Count - 1]
-            );
-        }
-    }
-
-
     private Vector3 GetRandomSpawnPosition()
     {
-        Renderer renderer = background.GetComponent<Renderer>();
-        float randomX = Random.Range(renderer.bounds.min.x, renderer.bounds.max.x);
-        float randomY = Random.Range(renderer.bounds.min.y, renderer.bounds.max.y);
+        if (_background == null)
+        {
+            Debug.LogError("Background object is not assigned.");
+            return Vector3.zero;
+        }
+
+        var renderer = _background.GetComponent<Renderer>();
+        if (renderer == null)
+        {
+            Debug.LogError("Renderer not found on the background object.");
+            return Vector3.zero;
+        }
+
+        var randomX = Random.Range(renderer.bounds.min.x, renderer.bounds.max.x);
+        var randomY = Random.Range(renderer.bounds.min.y, renderer.bounds.max.y);
         return new Vector3(randomX, randomY, 0);
     }
 }
