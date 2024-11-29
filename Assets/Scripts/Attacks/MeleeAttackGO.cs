7fbfb8f577be8705ad35eab18e4797fde9c1b931
JnsRdnp <112471004+JnsRdnp@users.noreply.github.com>
2024-11-29 17:13:42 +0200

    Attack prefab is destroyed after animation

--- Assets/Scripts/Attacks/MeleeAttackGO.cs
+++ Assets/Scripts/Attacks/MeleeAttackGO.cs
@@ -3,14 +3,29 @@ using UnityEngine;
 public class MeleeAttackGO : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Animator meleeAttackAnimator;
+
     void Start()
     {
-        
+        meleeAttackAnimator = GetComponent<Animator>();
+        Attack();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    // This activated from event at the end of the animation
+    public void DestroyAfterAnimation()
     {
-        
+        Destroy(gameObject);
     }
+
+    void Attack()
+    {
+        meleeAttackAnimator.Play("Melee attack");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Melee weapon hit something");
+    }
+
 }
