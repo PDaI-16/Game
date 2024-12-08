6e2f4ced40009cb3a2d33bb32aa7510283bd613d
JnsRdnp <112471004+JnsRdnp@users.noreply.github.com>
2024-12-08 14:03:58 +0200

    Settext refactor

--- Assets/Scripts/Inventory/ItemSlotScript.cs
+++ Assets/Scripts/Inventory/ItemSlotScript.cs
@@ -1,23 +1,18 @@
-using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ItemSlotScript : MonoBehaviour
 {
-
     [SerializeField] private Weapon weapon = null;
     [SerializeField] private Hat hat = null;
 
-
     void Start()
     {
-       
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
     }
 
     public void SetWeapon(Weapon weapondata)
@@ -27,11 +22,11 @@ public class ItemSlotScript : MonoBehaviour
             weapon = weapondata;
         }
 
-        SetAttackSpeedText(weapondata.AttackSpeed);
-        SetDamageText(weapondata.Damage);
+        // Call the modular SetText function for attack speed, damage, and category
+        SetText("AttackSpeedText", weapondata.AttackSpeed.ToString("F2"));
+        SetText("DamageText", weapondata.Damage.ToString("F2"));
+        SetText("ItemCategoryText", weapondata.Category.ToString());
         SetImage(weapondata.Sprite);
-        SetCategoryText(weapondata.Category);
-       
     }
 
     public Weapon GetWeapon()
@@ -39,38 +34,16 @@ public class ItemSlotScript : MonoBehaviour
         return weapon;
     }
 
-    /*    public void SetHat(Hat hatdata)
-        {
-            if (weapon == null)
-            {
-                hat = hatdata;
-            }
-
-        }
-
-        public Hat GetHat()
-        {
-            return hat;
-        }
-    */
-
-
-
     private void SetImage(Sprite itemImage)
     {
-        // Find the child GameObject with the name "ItemImage"
         Transform itemImageTransform = transform.Find("ItemImage");
 
-        // Check if the GameObject is found
         if (itemImageTransform != null)
         {
-            // Get the Image component from the found GameObject
             Image imageComponent = itemImageTransform.GetComponent<Image>();
 
-            // Check if the Image component is found
             if (imageComponent != null)
             {
-                // Set the sprite of the Image component
                 imageComponent.sprite = itemImage;
             }
             else
@@ -84,88 +57,27 @@ public class ItemSlotScript : MonoBehaviour
         }
     }
 
-    private void SetCategoryText(ItemCategory category)
+    // Modular method to set the text for any UI element
+    private void SetText(string childName, string textValue)
     {
-        // Find the child GameObject with the name "AttackSpeedText"
-        Transform categoryTextTransform = transform.Find("ItemCategoryText");
+        Transform textTransform = transform.Find(childName);
 
-        // Check if the GameObject is found
-        if (categoryTextTransform != null)
+        if (textTransform != null)
         {
-            // Get the TextMeshProUGUI component from the found GameObject
-            TextMeshProUGUI textObject = categoryTextTransform.GetComponent<TextMeshProUGUI>();
+            TextMeshProUGUI textObject = textTransform.GetComponent<TextMeshProUGUI>();
 
-            // Check if the TextMeshProUGUI component is found
             if (textObject != null)
             {
-                // Modify the text of the TextMeshProUGUI component
-                textObject.text = category.ToString();
+                textObject.text = textValue;
             }
             else
             {
-                Debug.LogWarning("No TextMeshProUGUI component found in AttackSpeedText.");
+                Debug.LogWarning($"No TextMeshProUGUI component found in {childName}.");
             }
         }
         else
         {
-            Debug.LogWarning("No child named 'AttackSpeedText' found in itemSlotObject.");
+            Debug.LogWarning($"No child named '{childName}' found in itemSlotObject.");
         }
     }
-
-    private void SetDamageText(float damage)
-    {
-        // Find the child GameObject with the name "AttackSpeedText"
-        Transform damageTextTransform = transform.Find("DamageText");
-
-        // Check if the GameObject is found
-        if (damageTextTransform != null)
-        {
-            // Get the TextMeshProUGUI component from the found GameObject
-            TextMeshProUGUI attackSpeedTextMeshPro = damageTextTransform.GetComponent<TextMeshProUGUI>();
-
-            // Check if the TextMeshProUGUI component is found
-            if (attackSpeedTextMeshPro != null)
-            {
-                // Modify the text of the TextMeshProUGUI component
-                attackSpeedTextMeshPro.text = damage.ToString("F2");
-            }
-            else
-            {
-                Debug.LogWarning("No TextMeshProUGUI component found in AttackSpeedText.");
-            }
-        }
-        else
-        {
-            Debug.LogWarning("No child named 'AttackSpeedText' found in itemSlotObject.");
-        }
-    }
-
-    private void SetAttackSpeedText(float attackspeed)
-    {
-        // Find the child GameObject with the name "AttackSpeedText"
-        Transform attackSpeedTextTransform  = transform.Find("AttackSpeedText");
-
-        // Check if the GameObject is found
-        if (attackSpeedTextTransform != null)
-        {
-            // Get the TextMeshProUGUI component from the found GameObject
-            TextMeshProUGUI attackSpeedTextMeshPro = attackSpeedTextTransform.GetComponent<TextMeshProUGUI>();
-
-            // Check if the TextMeshProUGUI component is found
-            if (attackSpeedTextMeshPro != null)
-            {
-                // Modify the text of the TextMeshProUGUI component
-                attackSpeedTextMeshPro.text = attackspeed.ToString("F2");
-            }
-            else
-            {
-                Debug.LogWarning("No TextMeshProUGUI component found in AttackSpeedText.");
-            }
-        }
-        else
-        {
-            Debug.LogWarning("No child named 'AttackSpeedText' found in itemSlotObject.");
-        }
-    }
-
 }
