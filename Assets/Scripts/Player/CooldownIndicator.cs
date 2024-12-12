9c3da6859d693fec78177b369b998941455127d3
JnsRdnp <112471004+JnsRdnp@users.noreply.github.com>
2024-12-12 02:58:37 +0200

    cool down fixes

--- Assets/Scripts/Player/CooldownIndicator.cs
+++ Assets/Scripts/Player/CooldownIndicator.cs
@@ -5,6 +5,7 @@ public class CooldownIndicator : MonoBehaviour
 {
     [SerializeField] private Slider cooldownSlider;
     [SerializeField] private PlayerController playerController;
+    [SerializeField] private GameObject fill;
 
 
     private float previousPercent = 0.0f;
@@ -25,9 +26,24 @@ public class CooldownIndicator : MonoBehaviour
         // Calculate the health percentage
         float cooldownPercent = playerController.GetCoolDownTimer() / playerController.GetCoolDownTime();
 
-        if(cooldownPercent == previousPercent)
+        if (cooldownPercent == 1)
+        {
+            cooldownPercent = 0;
+        }
+
+        if (cooldownPercent == previousPercent)
         {
             return;
+        }
+
+        Debug.LogWarning("Cooldown percent: "+cooldownPercent);
+
+        if (cooldownPercent <= 0.0f && fill.activeSelf == true) {
+            fill.SetActive(false);
+        }
+        else if (cooldownPercent > 0.0f && fill.activeSelf == false)
+        {
+            fill.SetActive(true);
         }
 
         previousPercent = cooldownPercent;
